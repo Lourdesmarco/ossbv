65d5ae1fcd3d81ff8bc841ddab43dab206b2dc97
Lourdes <lourdesmarco.design@gmail.com>
2021-05-16 21:07:22 +0200

    Add some sliders

--- sketch.js
+++ sketch.js
@@ -1,29 +1,81 @@
-let x = 700;
-let y = 250;
-let radio = 120;
-
 let audio;
-let A;
+let labelSize;
+let sliderSize;
+
+let labelRepeat;
+let sliderRepeat;
 
 function preload() {
   audio = loadSound('assets/audio/test.mp3');
-  //audio.onended(END)
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight - 100);
-  ellipseMode(RADIUS);
-  background(204);
-
+	createCanvas(windowWidth, windowHeight - 100);
+	setupAudio();
+  
   audio.play();
-  A = new p5.Amplitude();
-  A.setInput(audio);
+  
+  fill(150);
+  noStroke();
+
+  labelSize = createElement('label', 'Tamaño');
+  sliderSize = createSlider(10,200, 47);
+
+  labelRepeat = createElement('label', 'Repetición');
+  sliderRepeat = createSlider(1,20, 1);
 }
 
 function draw() {
+	updateAudio();
+	background(255);
 
-  ellipse(x, y, A.getLevel(), radio);
+  // for(let i = 0; i < 1; i++) {
+
+  // }
+
+   
+	for(let i = 0; i < sliderRepeat.value(); i++) {
+    
+
+		let freq = fft[i];
+		let x = map(i, 0, fft.length, 0, width);
+		let w = width / fft.length;
+		//rect(x, height, w, -freq);    
+    ellipse(sliderSize.value() * i, height/2, sliderSize.value() + freq, sliderSize.value() + freq)
+	}
+
+
 }
+
+/* AUDIO INIT */
+let mic, fftRaw, fft = [],
+	waveform = [],
+	amp = 0.0,
+	ampStereo = {
+		l: 0.0,
+		r: 0.0
+	},
+	numBins = 512,
+	bands = 12;
+
+function setupAudio() {
+	userStartAudio();
+	mic = new p5.AudioIn();
+	mic.start();
+	fftRaw = new p5.FFT(0.75, numBins);
+	fftRaw.setInput(mic);
+}
+
+function updateAudio() {
+	fftRaw.analyze();
+	amp = mic.getLevel() * 1000; // average mixed amplitude
+	ampStereo.l = mic.amplitude.getLevel(0) * 500; // average left amplitude
+	ampStereo.r = mic.amplitude.getLevel(1) * 500; // average right amplitude
+	waveform = fftRaw.waveform(); // array (-1, 1)
+	fft = fftRaw.logAverages(fftRaw.getOctaveBands(bands)); // array (0, 255)
+
+}
+
 
 
 
